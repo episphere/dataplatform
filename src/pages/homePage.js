20445966f6e8f36693319340cff6c402ce6a1665
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2021-11-29 23:10:18 -0500

    Banner changes

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -60,7 +60,7 @@ export const infoDeckAfterLoggedIn = async () => {
     template += `
         <div class="secondary-bg padding-bottom-1rem">
             <div class="confluence-banner">
-                <img src="./static/images/hero-banner.jpg" class="banner-logo" style="min-height: 120px;" alt="Confluence logo">
+                <img src="./static/images/nci-vol-10623-150.jpg" class="banner-logo" style="min-height: 120px;" alt="Confluence logo">
                 <div class="banner-overlay-text row justify-content-center text-center">
                     <div class="col-md-12">
                         <h1 class="banner-overlay-h1">BREAST CANCER RISK PREDICTION DATA PLATFORM</h1>
