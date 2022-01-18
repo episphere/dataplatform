31c1c3ed4656a23e0e7df0b3ab5d6ff3b4b6bb1b
Lee, Joshua <82845425+joshuleedel@users.noreply.github.com>
2022-01-18 14:12:50 -0500

    Removed dropdown for Analyze in nav bar:

--- src/components/navBarMenuItems.js
+++ src/components/navBarMenuItems.js
@@ -44,9 +44,6 @@ export const navBarMenutemplate = () => {
                 </a>
                 <div id="governanceNav" class="grid-elements"></div>
                 <div id="myProjectsNav" class="grid-elements"></div>
-                <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#data_analysis" title="Data Analysis" id="dataAnalysis">
-                    Analyze
-                </a>
                 <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#data_access" title="Data Access" id="dataRequest">
                     Request
                 </a>
