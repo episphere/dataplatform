d0e0e25520604e8854063b1561e2e3c4731a98fb
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2022-03-08 09:43:29 -0500

    Updated graph titles.

--- src/variables.js
+++ src/variables.js
@@ -180,7 +180,7 @@ export const variables = {
             0: 'negative',
             1: 'positive',
             888: "Don't Know",
-            'label': 'Estrogen receptor status'
+            'label': 'Estrogen Receptor Status'
         },
         eduCat: {
             'label': 'Highest level of education received'
@@ -315,7 +315,7 @@ export const variables = {
             2: 'other methods of detection',
             777: 'Not Applicable',
             888: "Don't Know",
-            'label': 'Mode of detection of first breast cancer diagnosis'
+            'label': 'Mode of Detection of First Breast Cancer Diagnosis'
         },
         Index: {
             1: 'ascertained for first tumour',
@@ -352,7 +352,7 @@ export const variables = {
             2: 'moderately differentiated',
             3: 'poorly/un-differentiated',
             888: "Don't Know",
-            'label': 'Histopathological grade'
+            'label': 'Histopathological Grade'
         },
         Grade2: {
             1: 'well differentiated',
