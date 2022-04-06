5a067c5786de4bf9af240b20bb631fdc0e90948c
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2022-04-06 16:57:53 -0400

    Updated roles for testing

--- src/shared.js
+++ src/shared.js
@@ -1209,9 +1209,9 @@ export const numberWithCommas = (x) => {
 
 export const emailsAllowedToUpdateData = ['patelbhp@nih.gov', 'ahearntu@nih.gov', 'ajayiat@nih.gov']
 
-export const emailforChair = ['kopchickbp@nih.gov']//['ahearntu@nih.gov', 'garciacm@nih.gov', 'kopchickbp@nih.gov', 'mia.gaudet@nih.gov', 'mukopadhyays2@nih.gov']//, 'ahearntu@nih.gov', 'wraynr@nih.gov']
+export const emailforChair = ['ahearntu@nih.gov', 'garciacm@nih.gov', 'mia.gaudet@nih.gov', 'mukopadhyays2@nih.gov','kopchickbp@nih.gov']//['ahearntu@nih.gov', 'garciacm@nih.gov', 'kopchickbp@nih.gov', 'mia.gaudet@nih.gov', 'mukopadhyays2@nih.gov']//, 'ahearntu@nih.gov', 'wraynr@nih.gov']
 
-export const emailforDACC = ['kopchickbp@nih.gov', 'wraynr@nih.gov']//['kopchickbp@nih.gov', 'garciacm@nih.gov', 'wraynr@nih.gov', 'mukopadhyays2@nih.gov', 'ahearntu@nih.gov', 'mia.gaudet@nih.gov']//,'wraynr@nih.gov',  'garciacm@nih.gov', 'mukopadhyays2@nih.gov']
+export const emailforDACC = ['garciacm@nih.gov','mukopadhyays2@nih.gov', 'ahearntu@nih.gov', 'mia.gaudet@nih.gov']//['kopchickbp@nih.gov', 'garciacm@nih.gov', 'wraynr@nih.gov', 'mukopadhyays2@nih.gov', 'ahearntu@nih.gov', 'mia.gaudet@nih.gov']//,'wraynr@nih.gov',  'garciacm@nih.gov', 'mukopadhyays2@nih.gov']
 
 export const publicDataFileId = 697309514903; //Unknown
 
