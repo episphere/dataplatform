63a686bca9c1e0edd61fe674a12814cf52dd496a
Navado Wray <nwray@deloitte.com>
2022-05-19 12:10:27 -0400

    update logout funtion

--- src/manageAuthentication.js
+++ src/manageAuthentication.js
@@ -54,7 +54,11 @@ export const logOut = async () => {
     const response = await fetch(`https://api.box.com/oauth2/revoke`, {
         method: 'POST',
         mode: 'no-cors',
-        body: `token=${access_token}&client_id=${clt.client_id}&client_secret=${clt.server_id}`
+        body: `token=${access_token}&client_id=${clt.client_id}&client_secret=${clt.server_id}`,
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+        },
+
     });
     delete localStorage.parms;
     // location.reload();
