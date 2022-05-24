9e69404182ddb688696343a600109b99e772ce44
Navado Wray <nwray@deloitte.com>
2022-05-24 06:55:34 -0400

    Uncomment Subset Statisitics

--- src/components/navBarMenuItems.js
+++ src/components/navBarMenuItems.js
@@ -27,7 +27,7 @@ export const navBarMenutemplate = () => {
                 <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links pl-4" href="#data_exploration/summary" title="Summary Statistics" id="dataSummary">
                     Summary Statistics
                 </a>
-                <!---${
+                <${
                     location.origin.match(applicationURLs.prod) ? 
                     ``:
                     `
@@ -35,7 +35,7 @@ export const navBarMenutemplate = () => {
                             Subset Statistics
                         </a>
                     `
-                }--->
+                }
                 
                 <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links pl-4" href="#data_exploration/dictionary" title="Data Dictionary" id="dataDictionary">
                     Dictionary
