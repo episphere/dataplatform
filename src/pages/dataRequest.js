cd904bf1b2e708d7c180a95840f84dc01d6be765
Navado Wray <nwray@deloitte.com>
2022-07-15 08:12:01 -0400

    Create unique filename upon upload

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -1233,7 +1233,20 @@ export const dataApproval = () => {
 export const dataForm = async () => {
   let files = await getFolderItems(uploadFormFolder);
   const d = new Date();
-  const filename = JSON.parse(localStorage.parms).login.split("@")[0] + "testing" + "_" + d.getDate() + "_" + (d.getMonth() + 1) + "_" + d.getFullYear() + ".docx";
+  let filename = JSON.parse(localStorage.parms).login.split("@")[0] + "testing" + "_" + d.getDate() + "_" + (d.getMonth() + 1) + "_" + d.getFullYear() + ".docx";
+  
+  // Find unique name
+  let entries = files.entries;
+  let i = 1;
+  while(entries.includes(filename)){
+    console.log('Chaning file name')
+    let indexOfExtension = filename.indexOf('.');
+    filename = filename.substring(0, indexOfExtension) + `(${i})` + filename.substring(indexOfExtension);
+    console.log('File name changed', filename);
+    i++;
+
+  }
+  
   const filesinfoldernames = [];
   const filesinfolderids = [];
   for (let i = 0; i < files.entries.length; i++) {
@@ -1505,27 +1518,47 @@ export const dataForm = async () => {
       //let files = getFolderItems(uploadFormFolder);//149098174998);
       if (filesinfoldernames.includes(filename)) {
         console.log(filename + " Exists: Saving New Version");
-        // const path = filename.split('.');
-        // let i = 0;
-        // console.log(path);
+        const [name, extension] = filename.split('.');
+        let i = 1;
+        console.log(name);
         
-        // while(filesinfoldernames.includes(filename)){
-        //   filename = path[0] + `(${i}).` + path[1];
-        //   i++; 
-        // }
-        let fileidupdate = filesinfolderids[filesinfoldernames.indexOf(filename)];
-        (async () => {
-          // document.getElementById('modalBody').innerHTML = 'Would'
-          let response = await uploadWordFileVersion(blob, fileidupdate);
-          await assigntasktochair();
-          document.getElementById('modalBody').innerHTML = `
-          <p>File was successfully updated.</p>
-          <p>Document ID: ${fileidupdate}</p>
-          
-          `;
-          $('#popUpModal').modal('show');
+        while(filesinfoldernames.includes(filename)){
+          console.log(filename, 'coming in')
+          console.log(filename.indexOf(')') - 1);
+          if(filename.includes(')')){
 
+            const [name, version] = filename.split('(');
+            filename = name + `(${i})` + version.substring(2,);
+          }
+          else {
+          filename = name + `(${i}).` + extension;
+          }
+          console.log('New name', filename);
+          i++; 
+        }
+        (async () => {
+          let response = await uploadWordFile(blob, filename, uploadFormFolder);
+          await assigntasktochair();
+          let fileid = response.entries[0].id;
+          //Modal code here
+          document.getElementById('modalBody').innerHTML = `
+          <p>File was successfully uploaded.</p>
+          <p>Document ID: ${fileid}</p>`;
+          $('#popUpModal').modal('show');
         })();
+        // let fileidupdate = filesinfolderids[filesinfoldernames.indexOf(filename)];
+        // (async () => {
+        //   // document.getElementById('modalBody').innerHTML = 'Would'
+        //   let response = await uploadWordFileVersion(blob, fileidupdate);
+        //   await assigntasktochair();
+        //   document.getElementById('modalBody').innerHTML = `
+        //   <p>File was successfully updated.</p>
+        //   <p>Document ID: ${fileidupdate}</p>
+          
+        //   `;
+        //   $('#popUpModal').modal('show');
+
+        // })();
       } else {
         console.log("Saving File to Box: " + filename + jsondata.keywords); // Adding keywords
         (async () => {
