d96eef394aaad7ccefb100293aaf80dea4972bb6
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2023-05-09 11:04:15 -0400

    updated tabs

--- src/components/navBarMenuItems.js
+++ src/components/navBarMenuItems.js
@@ -44,7 +44,7 @@ export const navBarMenutemplate = () => {
             <div class="dropdown-menu navbar-dropdown" aria-labelledby="navbarDropdown">
             <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#uploadinstruction" id="instructionID">How to upload data from my published manuscript </a>
             <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#myDCEG/upload" id="myDCEGID_upload"> Upload new data</a>
-            <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#data_governance" id="dataGovernance">See my uploaded data</a>
+            <a class="dropdown-item nav-link nav-menu-links dropdown-menu-links" href="#myDCEG" id="myDCEGID">See my uploaded data</a>
             <div id="governanceNav" class="grid-elements"></div>
 
             </div>
