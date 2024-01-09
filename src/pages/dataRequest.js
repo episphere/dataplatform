fb6ad9979b37e4dc24ab1f3f9e6222c2a70f1b52
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2024-01-09 10:36:15 -0500

    added automatic values from local storage

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -433,16 +433,16 @@ template += `
             </div>
             <div class="input-group">
               <label for="investigators"><b>Contact Investigator(s)</b> <span class='required-label'>*</span></label>
-              <input id="investigators" name="investigators" type="text" required/>
+              <input id="investigators" name="investigators" type="text" value="${JSON.parse(localStorage.parmsERa).name}" required/>
             </div>
             <div class="input-group">
               <label for="institution"><b>Institution(s)</b><span class='required-label'>*</span></label>
-              <input id="institution" name="institution" type="text" required/>
+              <input id="institution" name="institution" type="text" value="${JSON.parse(localStorage.parmsERa).company}" required/>
             </div>
               
             <div class="input-group">
               <label for="email"><b>Contact Email</b><span class='required-label'>*</span></label>
-              <input id="email" name="email" type="email" required/>
+              <input id="email" name="email" type="email" value="${JSON.parse(localStorage.parmsERa).email}" required/>
             </div>
             <!---<div class="input-group">
               <label for="member"> <b>Are you a member of DCEG?</b> <span class='required-label'>*</span></label>
