eb7c4319f9cb3fe2d9e93f253dea22295547afb0
bkop-ds <84399410+bkop-ds@users.noreply.github.com>
2024-01-09 10:47:49 -0500

    update logic

--- confluence.js
+++ confluence.js
@@ -305,12 +305,17 @@ export const confluence = async () => {
           // document
           //   .getElementById("amendmentno")
           //   .addEventListener("click", amendFormSelect);
-          
+          // if (!eraLogin) {
+          //   document.getElementById("loginERa").addEventListener("click", async function () {
+          //     location.href = `https://stsstg.nih.gov/auth/oauth/v2/authorize?response_type=code&client_id=ff775e46-ec74-46a3-b19f-ee2c60e8cf11&redirect_uri=https://episphere.github.io/dataplatform/&scope=openid+company+email+profile`
+          //   });
+          // }
+          if (eraLogin) {
+            await dataForm();
+          } else {
             document.getElementById("loginERa").addEventListener("click", async function () {
               location.href = `https://stsstg.nih.gov/auth/oauth/v2/authorize?response_type=code&client_id=ff775e46-ec74-46a3-b19f-ee2c60e8cf11&redirect_uri=https://episphere.github.io/dataplatform/&scope=openid+company+email+profile`
             });
-          if (eraLogin) {
-            await dataForm();
           }
         } else {
           confluenceDiv.innerHTML = await formSectionOther("form");
